f0ec6e2bfb06bdf18dfffd4caa434d78fa63d8e6
Lucas Moreira <eskerlucas1@gmail.com>
2024-04-06 15:52:00 -0300

    add ctrl p remapping

--- lua/fauker/remap.lua
+++ lua/fauker/remap.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<leader>p", "\"_dP")
 
 
 vim.opt.nu = true
