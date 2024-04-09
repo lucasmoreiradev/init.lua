b0e0ae52c5610cb8b59b1ca6292c53d53a029e2b
Lucas Moreira <eskerlucas1@gmail.com>
2024-04-09 11:54:05 -0300

    feat(colorsscheme): add colorscheme config

--- init.lua
+++ init.lua
@@ -793,6 +793,6 @@ cmp.setup {
     { name = 'path' },
   },
 }
--- vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme rose-pine")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
